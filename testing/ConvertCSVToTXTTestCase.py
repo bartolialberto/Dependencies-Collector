f988dd84eb8aaea9ab6b785201c335b6842890f8
LemmeeD <fabbio.dev@outlook.com>
2022-02-03 13:39:32 +0100

    Fix conversione .csv <--> .txt

--- testing/ConvertCSVToTXTTestCase.py
+++ testing/ConvertCSVToTXTTestCase.py
@@ -1,6 +1,7 @@
 import unittest
 from pathlib import Path
 from persistence.BaseModel import project_root_directory_name
+from utils import file_utils
 
 
 class ConvertCSVToTXTTestCase(unittest.TestCase):
@@ -17,21 +18,24 @@ class ConvertCSVToTXTTestCase(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.csv_filename = 'websites_us'
         cls.new_txt_filename = 'websites_us'
+        cls.PRD = ConvertCSVToTXTTestCase.get_project_root_folder()
 
     def test_conversion(self):
         new_lines = list()
-        f = open(str(self.csv_filename+".csv"), "r")
-        for line in f:
+        input_file = file_utils.set_file_in_folder('input', self.csv_filename+".csv", self.PRD)
+        f_in = open(str(input_file), "r")
+        for line in f_in:
             if line.startswith('www.'):
                 new_lines.append(line[4:])
             else:
                 new_lines.append(line)
-        f.close()
+        f_in.close()
 
-        f = open(str(self.new_txt_filename+".txt"), "r")
-        for line in f:
-            f.write(line)
-        f.close()
+        new_file = file_utils.set_file_in_folder('output', self.new_txt_filename + ".txt", self.PRD)
+        with open(str(new_file), "w") as f_out:
+            for line in new_lines:
+                f_out.write(line)
+            f_out.close()
 
 
 if __name__ == '__main__':
